d46302d97472be676227a0d81d24d170b7140175
DariuszWawryszuk <44153798+DariuszWawryszuk@users.noreply.github.com>
2019-01-16 14:17:43 +0100

    poprawka delete

--- 2hotels-management/src/main/java/com/hotels/hotelsmanagement/customer/repository/CustomerRepository.java
+++ 2hotels-management/src/main/java/com/hotels/hotelsmanagement/customer/repository/CustomerRepository.java
@@ -2,8 +2,10 @@ package com.hotels.hotelsmanagement.customer.repository;
 
 import com.hotels.hotelsmanagement.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
@@ -16,7 +18,10 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Iterable<Customer> findByFirstName(String firstName);
 
     Iterable<Customer> findByLastName(String lastName);
-
+    
+    @Transactional
+    @Modifying
+    @Query("update Customer c set c.active = false where c.id = ?1")
     void delete(Long id);
 }
 
